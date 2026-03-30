c87c87754f027cebdff3fcf6f943c97b3b422f09
offline2099 <offline2099@protonmail.com>
2026-03-30 12:06:43 +0700

    make data sections collapsible on tablet and mobile

--- src/components/03-celestial-object-data-container/CelestialObjectDataContainer.tsx
+++ src/components/03-celestial-object-data-container/CelestialObjectDataContainer.tsx
@@ -1,17 +1,30 @@
 import './CelestialObjectDataContainer.scss';
+import { useState } from 'react';
 import { HintType } from '../../constants/ui/hint-type.enum';
 import { ENTITY_TYPE_NAME } from '../../constants/data/entity-type-name';
 import { CelestialEntityData } from '../../types/ui/celestial-entity-data.interface';
+import { CelestialObjectDataSection } from '../../types/ui/celestial-object-data-section.interface';
 import { objectData } from '../../services/celestial-object';
 import DataValueContainer from '../04-data-value-container/DataValueContainer';
 import HintContainer from '../05-hint-container/HintContainer';
 
 const CelestialObjectData = (data: CelestialEntityData) => {
+  const sections: CelestialObjectDataSection[] = objectData(data);
+  const [collapsedSections, setCollapsedState] = useState<Set<number>>(new Set(sections.keys()));
+  const toggleSection = (index: number) => setCollapsedState(prev => {
+    const next = new Set(prev);
+    if (next.has(index)) next.delete(index);
+    else next.add(index);
+    return next;
+  });
   return (
     <div className={`data-container ${ENTITY_TYPE_NAME[data.type]}`}>
-      {objectData(data).map((section, sectionIndex) => 
-        <div key={sectionIndex} className='data-section'>
-          <div className='data-section-header'>{section.name}</div>
+      {sections.map((section, sectionIndex) => 
+        <div key={sectionIndex} className={`data-section${collapsedSections.has(sectionIndex) ? ' collapsed' : ''}`}>
+          <div className='data-section-header' onClick={() => toggleSection(sectionIndex)}>
+            {section.name}
+            <span className='section-chevron'>&#10094;</span>
+          </div>
           <div className='data-section-body'>
             {section.groups.map((group, groupIndex) => 
               <div key={groupIndex} className='data-section-group'>
